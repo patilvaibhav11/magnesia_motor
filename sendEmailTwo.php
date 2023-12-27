d42712eb147c8e060a797f08b202438992fd5406
patilvaibhav11 <patilvaibhav1209@gmail.com>
2023-12-27 14:22:03 +0530

    Code Updated

--- sendEmailTwo.php
+++ sendEmailTwo.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@speedmaxx.co.in";
+        $recipient = "manas.deo@engeniuspark.in";
 
         // Set the email subject.
         $subject = "New contact from $name";
