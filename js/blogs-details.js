b0c88b0b52b3129cc4b934a91ea1509ee879bbea
patilvaibhav11 <patilvaibhav1209@gmail.com>
2024-01-07 19:11:37 +0530

    Code Updated

--- js/blogs-details.js
+++ js/blogs-details.js
@@ -4,7 +4,7 @@ const blogId = urlParms.get("id");
 console.log(blogId);
 
 const blogData = {
-  c4ca4238a0b923820dcc509a6f75849b : `
+  c4ca4238a0b923820dcc509a6f75849b: `
             <div class="col-md-8 col-sm-7"> 
               <!-- single-blog start -->
               <article class="blog-post-wrapper">
@@ -51,35 +51,67 @@ const blogData = {
               <!-- single-blog end --> 
             </div>
   `,
-  c81e728d9d4c2f636f067f89cc14862c : `
-          <div class="col-md-8 col-sm-7"> 
-            <!-- single-blog start -->
-            <article class="blog-post-wrapper">
-              <div class="blog-banner"> <a href="javascript:void(0)" class="blog-images"> <img src="./images/blogs/blog2.jpg" alt=""> </a>
-                <div class="blog-content">
-                  <div class="blog-meta"> <span class="date-type"> 20 Dec 2023 </span> <span class="comments-type"> <i class="fa fa-comment-o"></i> 0 </span> </div>
-                  <a href="javascript:void(0)">
-                  <h4>Selection of Motor Power Rating</h4>
-                  </a>
-                  <p> Selection of power rating is important to achieve economy with reliability.   </p>
-                  <p>Improper selection of motor power rating results extra initial cost and extra loss of energy due to the operation below rated power makes the choice uneconomical.  </p>
-                  <p>Furthermore, induction and synchronous motors operate at a low power factor when operating below the rated power. </p>
-                  <p>During operation of the machine, heat is produced due to losses and temperature rises. An amount of developed heat is dissipated into the atmosphere. When the dissipation of heat is equal to the developed heat, then it is said to be equilibrium condition. "Motor temperature then reaches a steady state value.  </p>
-                  <p>Steady state temperature depends on power loss, which in turn depends on the output power of the machine. Since temperature rise has a direct relation with the output power, it is termed thermal loading on the machine. </p>
-                  <p>Steady state temperature varies in different parts of the machine. It is usually high ii the windings because loss density in conductors is high and dissipation is slow; and the conductors which are wrapped in insulating material are partly embedded in slots and thus are not directly exposed to the cooling air</p>
-                  <p>"Motor rating should be selected in such a way that the insulation temperature never exceeds the prescribed limit; otherwise it loses its thermal stability.  </p>
-                  <blockquote>
-                    <p>Empower Your Machines: Choose Precision, Choose Power, Choose the Right Motor Rating with Mangnesia.</p>
-                  </blockquote>
-                  <p>It is simple to calculate the motor power rating of the motor which operate at a constant power and speed. But most loads operate at variable power and speed at different applications, then exact Motor rating calculation becomes difficult.</p>
-                </div>
-              </div>
-            </article>
-            <div class="clear"></div>
-            <!-- single-blog end --> 
-          </div>
+  c81e728d9d4c2f636f067f89cc14862c: `
+  <div class="col-md-8 col-sm-7">
+  <!-- single-blog start -->
+  <article class="blog-post-wrapper">
+    <div class="blog-banner"> <a href="javascript:void(0)" class="blog-images"> <img
+          src="./images/blogs/blog2.jpg" alt=""> </a>
+      <div class="blog-content">
+        <div class="blog-meta"> <span class="date-type"> 20 Dec 2023 </span> <span class="comments-type"> <i
+              class="fa fa-comment-o"></i> 0 </span> </div>
+        <a href="javascript:void(0)">
+          <h4>Selection of Motor Power Rating</h4>
+        </a>
+        <ul>
+          <li>
+            <p>⦿ Selection of power rating is important to achieve economy with reliability.</p>
+          </li>
+          <li>
+           <p>⦿ Improper selection of motor power rating results extra initial cost and extra loss of energy due
+            to the operation below rated power makes the choice uneconomical.</p>
+          </li>
+          <li>
+            <p>⦿ Furthermore, induction and synchronous motors operate at a low power factor when operating below
+              the rated power.</p>
+          </li>
+          <li>
+           <p>⦿ During operation of the machine, heat is produced due to losses and temperature rises. An amount
+            of developed heat is dissipated into the atmosphere. When the dissipation of heat is equal to
+            the developed heat, then it is said to be equilibrium condition. "Motor temperature then reaches
+            a steady state value.</p>
+          </li>
+          <li>
+            <p>⦿ Steady state temperature depends on power loss, which in turn depends on the output power of the
+              machine. Since temperature rise has a direct relation with the output power, it is termed
+              thermal loading on the machine.</p>
+          </li>
+          <li>
+            <p>⦿ Steady state temperature varies in different parts of the machine. It is usually high ii the
+              windings because loss density in conductors is high and dissipation is slow; and the conductors
+              which are wrapped in insulating material are partly embedded in slots and thus are not directly
+              exposed to the cooling air</p>
+          </li>
+          <li>
+            <blockquote>
+              <p>"Motor rating should be selected in such a way that the insulation temperature never
+                exceeds the prescribed limit; otherwise it loses its thermal stability. </p>
+            </blockquote>
+          </li>
+          <li>
+           <p>⦿ It is simple to calculate the motor power rating of the motor which operate at a constant power
+            and speed. But most loads operate at variable power and speed at different applications, then
+            exact Motor rating calculation becomes difficult.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </article>
+  <div class="clear"></div>
+  <!-- single-blog end -->
+</div>
   `,
-  eccbc87e4b5ce2fe28308fd9f2a7baf3 : `<div class="col-md-8 col-sm-7"> 
+  eccbc87e4b5ce2fe28308fd9f2a7baf3: `<div class="col-md-8 col-sm-7"> 
   <!-- single-blog start -->
   <article class="blog-post-wrapper">
     <div class="blog-banner">
@@ -93,14 +125,14 @@ const blogData = {
   <div class="clear"></div>
   <!-- single-blog end --> 
 </div>`,
-  a87ff679a2f3e71d9181a67b7542122c : '4'
+  a87ff679a2f3e71d9181a67b7542122c: '4'
 };
 
 const blogElement = document.getElementById('blogDetail')
-if(blogData[blogId]){
-    blogElement.innerHTML = blogData[blogId]
-}else{
-    blogElement.innerHTML = `<div class="col-md-8 col-sm-7"> 
+if (blogData[blogId]) {
+  blogElement.innerHTML = blogData[blogId]
+} else {
+  blogElement.innerHTML = `<div class="col-md-8 col-sm-7"> 
     <!-- single-blog start -->
     <article class="blog-post-wrapper">
       <div class="blog-banner">
